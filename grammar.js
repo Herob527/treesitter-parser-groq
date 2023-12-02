feda3763eb473d01dd375d1cf67476d7dd7db93b
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-12-02 15:48:52 +0100

    Working on grammar and added node mon

--- grammar.js
+++ grammar.js
@@ -2,193 +2,103 @@
 
 // @ts-check
 
+/**
+ * Creates a rule to match one or more of the rules separated by a comma
+ *
+ * @param {Rule} rule
+ *
+ * @return {SeqRule}
+ *
+ */
+function commaSep1(rule) {
+  return seq(rule, repeat(seq(",", rule)));
+}
+
+/**
+ * Creates a rule to optionally match one or more of the rules separated by a comma
+ *
+ * @param {Rule} rule
+ *
+ * @return {ChoiceRule}
+ *
+ */
+function commaSep(rule) {
+  return optional(commaSep1(rule));
+}
 module.exports = grammar({
   name: "groq",
 
   rules: {
-    source_file: ($) => repeat(choice($.query, $.value, $.pair)),
-
     query: ($) =>
-      prec.left(
-        2,
-        choice(
-          $.nullary_access_operator,
-          $.arraylike,
-          $.pipe,
-          $.sort_order,
-          $.filter,
-        ),
-      ),
-    pair: ($) =>
-      prec.left(
-        1,
-        choice($.identifier, $.value, $.filter, seq("=>", $.pair), "ERROR"),
-      ),
-
-    variable: ($) => /\$[_A-Za-z][_0-9A-Za-z]*/,
-
-    keyword: ($) => choice("asc", "desc", "in", "match"),
-
-    comparison: ($) => /(?:==|!=|>=|<=|>|<)/,
-
-    operator: ($) => choice(/\+/, /\-/, /\*/, /\*\*/, /\//, /%/),
-
-    pipe: ($) => /\|/,
-
-    logical: ($) => /\!|\&\&|\|\|/,
-
-    reference: ($) => /->/,
-
-    range: ($) => seq(/\s*/, /\d+/, /(\.{2,3})/, /\d+/, /\s*/),
-
-    spread: ($) => seq("...", optional(".")),
-
-    array: ($) => prec.left(2, seq("[", repeat($.array_values), "]")),
-    arraylike: ($) =>
       seq(
-        "[",
-        optional(seq("*", ".", optional("."))),
-        optional($.arraylike_repeat1),
-        "]",
-        optional("."),
-      ),
-
-    array_values: ($) =>
-      prec.left(2, choice($.value, $.spread, ",", /[^\s\]]/)),
-    array_repeat1: ($) =>
-      prec.left(2, seq($.array_values, repeat1(seq(",", $.array_values)))),
-    arraylike_repeat1: ($) =>
-      prec.left(1, seq($.array_values, repeat1(seq(",", $.array_values)))),
-
-    named_projection: ($) =>
-      prec.left(
-        2,
-        choice(
-          seq($.objectkey, optional($.projection)),
-          seq($.objectkey, optional($.pair)),
-          seq($.objectkey), // handle the case where there's only an object key
+        repeat(
+          choice(
+            $.variable_identifier,
+            $.normal_identifier,
+            $.operator,
+            $.every,
+            $.string,
+            $.expression_identifier,
+            $.array_identifier,
+            $.object,
+          ),
         ),
       ),
 
-    projection_repeat1: ($) => prec.left(1, seq($.named_projection, $.pair)),
-    filter: ($) =>
-      prec.left(
-        2,
-        choice(
-          $.function_call,
-          $.keyword,
-          $.constant,
-          $.identifier,
-          $.value,
-          $.comparison,
-          $.operator,
-          $.logical,
-          "ERROR", // Add the possibility of an error in a filter
+    array_identifier: (_) => {
+      const alpha = /[_a-zA-Z]+?/;
+      const alphanumeric = /[_a-zA-Z\d]+?/;
+      return token(seq(alpha, repeat(alphanumeric), "[]"));
+    },
+
+    variable_identifier: (_) => {
+      const alpha = /[_a-zA-Z]+?/;
+      const alphanumeric = /[_a-zA-Z\d]+?/;
+      return token(seq("$", alpha, repeat(alphanumeric)));
+    },
+
+    normal_identifier: ($) => {
+      const alpha = /[_a-zA-Z]+?/;
+      const alphanumeric = /[_a-zA-Z\d]+?/;
+      return token(
+        seq(
+          "",
+          alpha,
+          repeat(alphanumeric),
+          optional(seq(".", alpha, repeat(alphanumeric))),
         ),
-      ),
-    value: ($) =>
-      prec.left(
-        3,
-        choice(
-          $.constant,
-          $.number,
-          $.string,
-          $.array,
-          $.variable,
-          $.projection,
-          $.function_call,
-        ),
-      ),
+      );
+    },
 
-    nullary_access_operator: ($) => /[*@^]/,
+    operator: (_) => token(choice("==", "||", "&&", "matches", "!=", "[]")),
 
-    constant: ($) => choice("true", "false", "null"),
+    every: () => token("*"),
 
-    number: ($) => /-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?/,
+    string: () => token(seq('"', /[\w]*?/, '"')),
 
-    projection: ($) =>
+    pair: ($) =>
+      seq(field("key", $.string), ":", field("value", $.expression_identifier)),
+
+    object: ($) =>
       seq(
         "{",
-        optional(","),
-        repeat(
-          seq(
-            choice($.identifier, $.objectkey, $.named_projection),
-            optional("..."),
-            optional($.pair),
-            seq(
-              ":",
-              choice(
-                $.nullary_access_operator,
-                $.arraylike,
-                $.value,
-                $.spread,
-                $.identifier,
-                $.operator,
-                $.comparison,
-                $.pair,
-              ),
-            ),
-          ),
+        commaSep(
+          optional(choice($.pair, $.normal_identifier, $.array_identifier)),
         ),
         "}",
       ),
 
-    string: ($) => choice($.double_string, $.single_string),
+    expression_identifier: ($) =>
+      seq(choice($.normal_identifier, $.every), $.expression),
 
-    double_string: ($) => seq('"', repeat(choice(/[^"\\]+/, /\\./)), '"'), // Updated to allow escape sequences
-
-    single_string: ($) => seq("'", repeat($.stringcontent), "'"),
-
-    objectkey: ($) => prec.left(2, $.string),
-
-    stringcontent: ($) => choice(/\\[\\\/bfnrt"]/, /\\u[0-9a-fA-F]{4}/, /\\./),
-
-    sort_pair: ($) =>
-      choice(
-        seq(/[_A-Za-z][_0-9A-Za-z]*/, optional(/\s*(asc|desc)/)),
-        seq("@", "[", $.string, "]", optional(/\s*(asc|desc)/)),
-      ),
-
-    sort_order: ($) =>
-      seq("order", "(", $.sort_pair, repeat1(seq(",", $.sort_pair)), ")"),
-
-    _sort_pair: ($) => $.sort_pair,
-
-    function_call: ($) =>
-      choice($.function_var_arg, $.function_single_arg, $.function_round),
-
-    function_var_arg: ($) =>
+    expression_element: ($) =>
       seq(
-        choice("coalesce", "select"),
-        "(",
-        repeat1(choice($.value, $.identifier, $.filter, $.pair, ",")),
-        ")",
+        $.normal_identifier,
+        $.operator,
+        choice($.variable_identifier, $.string),
+        choice($.operator, ""),
       ),
 
-    function_single_arg: ($) =>
-      seq(
-        choice("count", "defined", "length", "path", "references"),
-        "(",
-        choice($.query, $.identifier, $.value, $.pair),
-        ")",
-      ),
-
-    function_round: ($) =>
-      seq(
-        choice("avg", "max", "min", "sum"),
-        "(",
-        choice($.query, $.identifier, $.value),
-        ")",
-      ),
-    identifier: ($) =>
-      choice(
-        seq(
-          choice(/[_A-Za-z][_0-9A-Za-z]*/, /[@^]/),
-          optional(/\s*\[\s*\]\s*/),
-          ".",
-        ),
-        /[_A-Za-z][_0-9A-Za-z]*/,
-      ),
-    error: ($) => /ERROR/,
+    expression: ($) => seq("[", repeat1($.expression_element), "]"),
   },
 });
